51a9930183b74b1078be84f6ad18d82f50456b33
whussein <whussein@jarnetsolutions.com>
2019-04-15 17:32:37 +0300

    add the socket config for client side and server side

---
+++ public/main.js
@@ -0,0 +1,2 @@
+// open the socket connection
+var socket = io.connect("http://localhost:4000");
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require("express");
+var socket = require("socket.io");
 
 // App Setup
 var app = express();
 
-app.use(express.static("public"));
-
 // Server listen Port
 var server = app.listen(4000, () => {
   console.log("App listening on port 4000!");
+});
+
+app.use(express.static("public"));
+
+//socket config
+var io = socket(server);
+
+// every client will have his own socket
+io.on("connection", socket => {
+  console.log("socket connection has made", socket.id);
 });
